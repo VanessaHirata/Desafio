6d299345a4c31cc30547f3f826f1689d55ecf9a8
VanessaHirata <156018851+VanessaHirata@users.noreply.github.com>
2024-03-13 18:14:00 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,22 @@
+let numberOne = 4;
+let numberTwo = 4;
+
+const soma = numberOne + numberTwo;
+console.log(soma);
+
+console.log(numberOne - numberTwo);
+
+console.log(numberOne * numberTwo);
+
+console.log(numberOne / numberTwo);
+
+console.log(numberOne % numberTwo);
+
+const par = soma % 2 === 0;
+console.log(par);
+ 
+const igual = numberOne === numberTwo;
+console.log(igual)
+
+
+
